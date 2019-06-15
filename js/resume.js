aa1e375b7841291b68f4e31133b1f90dcf3231b9
abishekarun <rajendranarun95@gmail.com>
2019-06-14 20:48:51 -0700

    sidebar

--- js/resume.js
+++ js/resume.js
@@ -29,18 +29,8 @@
 
 $(document).ready(function () {
 
-    $("#sideNav").mCustomScrollbar({
-         theme: "minimal"
-    });
-
     $('#sidebarCollapse').on('click', function () {
-        // open or close navbar
         $('#sideNav').toggleClass('active');
-        // close dropdowns
-        $('.collapse.in').toggleClass('in');
-        // and also adjust aria-expanded attributes we use for the open/closed arrows
-        // in our CSS
-        $('a[aria-expanded=true]').attr('aria-expanded', 'false');
     });
 
 });
