0d6459936607d18fb4eed956acf15dcad1564b73
abishekarun <rajendranarun95@gmail.com>
2019-06-14 20:39:19 -0700

    sidebar

--- js/resume.js
+++ js/resume.js
@@ -26,3 +26,21 @@
   });
 
 })(jQuery); // End of use strict
+
+$(document).ready(function () {
+
+    $("#sideNav").mCustomScrollbar({
+         theme: "minimal"
+    });
+
+    $('#sidebarCollapse').on('click', function () {
+        // open or close navbar
+        $('#sideNav').toggleClass('active');
+        // close dropdowns
+        $('.collapse.in').toggleClass('in');
+        // and also adjust aria-expanded attributes we use for the open/closed arrows
+        // in our CSS
+        $('a[aria-expanded=true]').attr('aria-expanded', 'false');
+    });
+
+});
